95e10403ea4020260b686e707e2716188d0bb0ac
tricarbonate <reibell.theo@gmail.com>
2021-07-30 19:30:40 -0400

    added zsh functions file :

---
+++ zsh/functions.zsh
@@ -0,0 +1,13 @@
+#!/usr/bin/env zsh
+
+
+# Own functions
+#
+
+
+# Function to start a problem on codeforces
+function cf-start() {
+  cd ~/Documents/tricarbonate/codeforces/contest
+  zsh start.sh
+  vim a.cpp
+}
