2ac3a2ee8ad17407faf601a685d03296c396e9ed
tricarbonate <reibell.theo@gmail.com>
2023-05-11 17:12:32 -0400

    nordic config + set cmdheight=0

--- stow/nvim-lua/lua/config/lualine.lua
+++ stow/nvim-lua/lua/config/lualine.lua
@@ -1,7 +1,7 @@
 require('lualine').setup({
     options = {
         icons_enabled = true,
-        theme = require('config/lualine-theme').theme(),
+        theme = 'nordic',
         -- theme = "catppuccin",
         always_divide_middle = true
     },
