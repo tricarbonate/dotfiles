6695c39086f4351b3b064e7ae575dd31f1336818
tricarbonate <reibell.theo@gmail.com>
2023-12-11 17:27:27 -0500

    added window navigation in vimscript

--- stow/nvim/lua/config/keymaps.lua
+++ stow/nvim/lua/config/keymaps.lua
@@ -26,6 +26,27 @@ keymap.set("n", "<leader>e", ":Telescope file_browser path=%:p:h select_buffer=t
 -- Add undo break-points
 keymap.set("i", "<cr>", "<cr><c-g>u")
 
+vim.cmd([[
+    function! WinMove(key)
+        let t:curwin = winnr()
+        exec "wincmd ".a:key
+        if (t:curwin == winnr())
+            if (match(a:key,'[jk]'))
+                wincmd v
+            else
+                wincmd s
+            endif
+            exec "wincmd ".a:key
+        endif
+    endfunction
+
+    nnoremap <leader>d :<C-W>q<CR>
+    nnoremap <silent> <C-h> :call WinMove('h')<CR>
+    nnoremap <silent> <C-j> :call WinMove('j')<CR>
+    nnoremap <silent> <C-k> :call WinMove('k')<CR>
+    nnoremap <silent> <C-l> :call WinMove('l')<CR>
+]])
+
 -- Changes or link a specific highlight group to another
 -- vim.api.nvim_set_hl(0, "@String", { link = "Comment" })
 
