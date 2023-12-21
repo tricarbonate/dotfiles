bf143ce3fb479a36085fea95950f1330a6d61292
tricarbonate <reibell.theo@gmail.com>
2023-12-20 21:35:42 -0500

    back to smooth scrolling + disabled some default lazyvim plugins

--- stow/nvim/lua/config/keymaps.lua
+++ stow/nvim/lua/config/keymaps.lua
@@ -13,9 +13,11 @@ keymap.set("n", "<leader>wl", ":vsplit<return>")
 -- Clears last highlight (already done by default with <esc> from lazyvim)
 -- keymap.set("n", "<C-m>", ":nohlsearch<CR>")
 
--- scroll up and down with m and U
-keymap.set("n", "m", "<C-d>")
-keymap.set("n", "U", "<C-U>")
+-- SCROLLING - using neoscroll and custom number of scrolled line (a bit less than default)
+-- keymap.set("n", "m", "<C-d>zz")
+keymap.set("n", "m", ":lua require('neoscroll').scroll(35, true, 1)<CR>zz")
+keymap.set("n", "U", ":lua require('neoscroll').scroll(-35, true, 1)<CR>zz")
+keymap.set("n", "zz", ":lua require('neoscroll').zz(1)<CR>")
 
 -- reload lua config file (might not entirely work, maybe restart neovim to make sure)
 keymap.set("n", "<leader>lr", ":luafile %")
@@ -25,6 +27,13 @@ keymap.set("n", "<leader>e", ":Telescope file_browser path=%:p:h select_buffer=t
 
 -- Add undo break-points
 keymap.set("i", "<cr>", "<cr><c-g>u")
+
+-- Neoscroll mappings (smooth scrolling)
+-- local t = {}
+-- t["U"] = { "scroll", { "-vim.wo.scroll", "true", "1" } }
+-- t["m"] = { "scroll", { "vim.wo.scroll", "true", "1" } }
+-- t["zz"] = { "zz", { "1" } }
+-- require("neoscroll.config").set_mappings(t)
 
 vim.cmd([[
     function! WinMove(key)
@@ -54,6 +63,8 @@ keymap.set("n", "gj", ':lua require("harpoon.ui").nav_file(1)<CR>')
 keymap.set("n", "gk", ':lua require("harpoon.ui").nav_file(2)<CR>')
 keymap.set("n", "gl", ':lua require("harpoon.ui").nav_file(3)<CR>')
 keymap.set("n", "g;", ':lua require("harpoon.ui").nav_file(4)<CR>')
+
+--[ DEFAULT KEYMAPS (NO PLUGINS) ]--
 
 -- Changes or link a specific highlight group to another
 -- vim.api.nvim_set_hl(0, "@String", { link = "Comment" })
--- stow/nvim/lua/plugins/noice.lua
+++ stow/nvim/lua/plugins/noice.lua
@@ -1,6 +1,8 @@
 return {
   {
     "folke/noice.nvim",
+    enabled = false,
+    view = "cmdline",
     opts = function(_, opts)
       opts.presets.lsp_doc_border = true
     end,
